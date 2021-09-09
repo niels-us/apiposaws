4b1c7be56bbba77e052b0ebc0022b6f98af98727
niels-us <niels_us@outlook.com>
2021-09-08 22:03:59 -0500

    deploy aws5

--- backpos/settings.py
+++ backpos/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #SECRET_KEY = ''
 SECRET_KEY = 'av8ocxogudjo3nh8-6xt60(_q810i%!)j)f6eco4)jss9k'  
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -173,5 +173,3 @@ cloudinary.config(
 )
 
 CORS_ORIGIN_ALLOW_ALL = True
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
